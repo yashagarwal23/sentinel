f52851693406b665078e9b317ef5325921a1358c
Yash Agarwal <yasha.co.17@nsit.net.in>
2019-03-14 16:04:33 +0530

    flask version except chkrt done

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIRECTORY=$(cd `dirname $0` && pwd)
+cd $DIRECTORY/sentinelfrontend
+source $DIRECTORY/venv/bin/activate
+$DIRECTORY/venv/bin/python3 $DIRECTORY/run.py & 
+
+sleep 5
+$DIRECTORY/venv/bin/python3 $DIRECTORY/sentinelfrontend/outputBaseScreen.py
+
